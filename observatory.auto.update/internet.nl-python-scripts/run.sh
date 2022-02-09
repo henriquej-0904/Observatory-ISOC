4bed7caf8dc70643466a49898ea1282b49faffd7
Henrique Campos Ferreira <hjp.ferreira@campus.fct.unl.pt>
2022-02-09 22:51:33 +0000

    Output json in python scripts

--- observatory.auto.update/internet.nl-python-scripts/run.sh
+++ observatory.auto.update/internet.nl-python-scripts/run.sh
@@ -4,4 +4,4 @@ mkdir -p results
 docker run --rm -it --network host -v "$(pwd)/domains:/internet.nl_batch_scripts/domains" \
 -v "$(pwd)/results:/internet.nl_batch_scripts/results" \
 -v "$(pwd)/batch.conf:/internet.nl_batch_scripts/batch.conf" \
--v "/etc/passwd:/etc/passwd:ro" -u $UID internet-nl-python-scripts $*
+-v "/etc/passwd:/etc/passwd:ro" internet-nl-python-scripts $*
