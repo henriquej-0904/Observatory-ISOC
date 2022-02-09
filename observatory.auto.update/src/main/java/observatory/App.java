02ac55c255d566ae6f8de09947d75d6881499c0a
Henrique Campos Ferreira <hjp.ferreira@campus.fct.unl.pt>
2022-02-09 17:23:38 +0000

    Add observatory auto update project config

---
+++ observatory.auto.update/src/main/java/observatory/App.java
@@ -0,0 +1,13 @@
+package observatory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
