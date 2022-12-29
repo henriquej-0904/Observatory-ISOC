a3a484e46ce3ac98d83442c4d868c242d7b46d23
henriquej-0904 <hjp.ferreira@campus.fct.unl.pt>
2022-12-29 18:57:10 +0000

    Run docker image latest

--- observatory/runDocker.sh
+++ observatory/runDocker.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 mkdir -p results
-docker run --rm -it -v "$(pwd)/config:/observatory/config" -v "$(pwd)/results:/observatory/results" henriquej0904/observatory-isoc:1.3.1 $*
+docker run --rm -it -v "$(pwd)/config:/observatory/config" -v "$(pwd)/results:/observatory/results" henriquej0904/observatory-isoc $*
